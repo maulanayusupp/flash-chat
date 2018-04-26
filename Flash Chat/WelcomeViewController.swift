bb14e4145e3a908ea5ff87812a4dc884a9867732
Maulana Yusup Abdullah <adeneling@gmail.com>
2018-04-26 08:41:49 +0700

    Init project

---
+++ Flash Chat/WelcomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat
+//  Followed by Angela Yu on 26/04/2018.
+//  This is the welcome view controller - the first thign the user sees
+//
+
+import UIKit
+
+
+
+class WelcomeViewController: UIViewController {
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
